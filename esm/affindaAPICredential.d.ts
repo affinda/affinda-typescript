b869a130c376acfbf37ee0a6c4bc2ae7004e09b2
Chris Culhane <cfculhane@gmail.com>
2021-08-11 13:25:50 +1000

    Remove ignored files

--- esm/affindaAPICredential.d.ts
+++
@@ -1,7 +0,0 @@
-import { AccessToken, GetTokenOptions, TokenCredential } from "@azure/identity";
-export declare class AffindaCredential implements TokenCredential {
-    token: string;
-    constructor(token: string);
-    getToken(scopes: string | string[], options?: GetTokenOptions): Promise<AccessToken | null>;
-}
-//# sourceMappingURL=affindaAPICredential.d.ts.map
