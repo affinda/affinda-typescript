fd6c143fbcdeb5d8e919621bad6b4b872a1ed84d
Chris <chris.culhane@affinda.com>
2021-08-11 13:15:38 +1000

    Remove ignored files

--- esm/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./models";
-export { AffindaAPI } from "./affindaAPI";
-export { AffindaAPIContext } from "./affindaAPIContext";
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./models";
-export { AffindaAPI } from "./affindaAPI";
-export { AffindaAPIContext } from "./affindaAPIContext";
