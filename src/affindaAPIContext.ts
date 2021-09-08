7f3e1e4ef0125219ffa54f4b5f9aecb0ee64ec8f
Chris Culhane <cfculhane@gmail.com>
2021-09-08 20:40:27 +1000

    Bump version

--- src/affindaAPIContext.ts
+++ src/affindaAPIContext.ts
@@ -29,7 +29,7 @@ export class AffindaAPIContext extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-affinda/0.2.2`;
+    const packageDetails = `azsdk-js-affinda/0.2.4`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
