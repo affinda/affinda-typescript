c832e54b84fe7e1b1a7ab3d45dc58510ce0a72fa
Chris Culhane <cfculhane@gmail.com>
2023-03-20 01:49:59 +0000

    #autocommit #release - 6.2.0

--- src/affindaAPIContext.ts
+++ src/affindaAPIContext.ts
@@ -27,7 +27,7 @@ export class AffindaAPIContext extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-affinda/6.1.0`;
+    const packageDetails = `azsdk-js-affinda/6.2.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
