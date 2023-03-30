e4c482472d602ce8e530c7420441fff276421f23
anthony-affinda <anthony.malkoun@affinda.com>
2023-03-30 12:04:07 +0000

    #autocommit #release - 5.3.1

--- src/affindaAPIContext.ts
+++ src/affindaAPIContext.ts
@@ -27,7 +27,7 @@ export class AffindaAPIContext extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-affinda/5.3.0`;
+    const packageDetails = `azsdk-js-affinda/5.3.1`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
