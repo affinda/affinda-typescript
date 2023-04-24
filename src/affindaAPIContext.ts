32a10bec682e56e35ef9af2fe5f9734a0eaefb37
Toby Zerner <toby.zerner@gmail.com>
2023-04-24 02:39:22 +0000

    #autocommit #release - 6.6.0

--- src/affindaAPIContext.ts
+++ src/affindaAPIContext.ts
@@ -27,7 +27,7 @@ export class AffindaAPIContext extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-affinda/6.5.2`;
+    const packageDetails = `azsdk-js-affinda/6.6.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
