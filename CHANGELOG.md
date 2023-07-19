8bb941dec9aa5c887df00db35337a9d11e532a17
kirstenbird <kirstenbird@gmail.com>
2023-07-19 02:31:34 +0000

    #autocommit #release - 6.16.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+
+## [6.16.0] - 2023-07-19
 ### Changed
 - Set CustomFieldConfig default to 0.5
 - Make Field.slug nullable and not required
--- src/affindaAPIContext.ts
+++ src/affindaAPIContext.ts
@@ -27,7 +27,7 @@ export class AffindaAPIContext extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-affinda/6.15.0`;
+    const packageDetails = `azsdk-js-affinda/6.16.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
