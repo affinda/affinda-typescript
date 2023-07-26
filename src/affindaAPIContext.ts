4ba0339d33c21bde82888a49afcadb343c6d2573
rockstarr-programmerr <rockstarrprogrammerr@gmail.com>
2023-07-26 09:28:48 +0000

    #autocommit #release - 6.18.0

--- src/affindaAPIContext.ts
+++ src/affindaAPIContext.ts
@@ -27,7 +27,7 @@ export class AffindaAPIContext extends coreClient.ServiceClient {
       credential: credentials
     };
 
-    const packageDetails = `azsdk-js-affinda/6.17.0`;
+    const packageDetails = `azsdk-js-affinda/6.18.0`;
     const userAgentPrefix =
       options.userAgentOptions && options.userAgentOptions.userAgentPrefix
         ? `${options.userAgentOptions.userAgentPrefix} ${packageDetails}`
